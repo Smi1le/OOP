2520ac6e2f0bbe9f1ada387ea90704ec0221e6b5
tima55510 <tima55510@gmail.com>
2016-03-19 08:55:06 +0300

    поправлен код

--- Lab2/Lab2_3/Lab2_3/ProcessorMap.cpp
+++ Lab2/Lab2_3/Lab2_3/ProcessorMap.cpp
@@ -5,27 +5,23 @@ using namespace std;
 
 string FindAndReplace(string const &subject, string const &search, string const &replace)
 {
-	cout << "sdsdsdsd" << endl;
-	if (subject.length() != 0)
-	{
-		string bufStr;
-		for (size_t i = 0; i < subject.length();)
-		{
-			if (i <= subject.length() - search.length() && subject.substr(i, search.length()) == search)
-			{
-				bufStr.append(replace);
-				i += search.length();
-			}
-			else
-			{
-				bufStr += subject[i];
-				++i;
-			}
-		}
-		return bufStr;
-	}
-	else
+	if (subject.length() == 0)
 	{
 		return subject;
 	}
+	string bufStr;
+	for (size_t i = 0; i < subject.length();)
+	{
+		if (i <= subject.length() - search.length() && subject.substr(i, search.length()) == search)
+		{
+			bufStr.append(replace);
+			i += search.length();
+		}
+		else
+		{
+			bufStr += subject[i];
+			++i;
+		}
+	}
+	return bufStr;
 }
