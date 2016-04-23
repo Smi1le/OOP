5acb5e396f60c83c85a8557e71aabd99595137de
Smi1le <tima55510@gmail.com>
2016-04-24 01:09:12 +0300

    Перегружены все операторы кроме оператора ввода из потока

---
+++ Lab5/lab5/task1/Rational.cpp
@@ -0,0 +1,279 @@
+﻿#include "stdafx.h"
+#include "Rational.h"
+#include <utility>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+
+
+CRational::CRational(int numerator, int denominator)
+	: m_numerator(numerator)
+	, m_denominator(denominator)
+{
+	if (denominator < 0)
+	{
+		m_numerator = -m_numerator;
+		m_denominator = -m_denominator;
+	}
+	Normalize();
+}
+
+int CRational::GetNumerator() const
+{
+	return m_numerator;
+}
+
+int CRational::GetDenominator() const
+{
+	return m_denominator;
+}
+
+void CRational::Normalize()
+{
+	const int gcd = GCD(abs(m_numerator), m_denominator);
+	m_numerator /= gcd;
+	m_denominator /= gcd;
+}
+
+unsigned GCD(unsigned a, unsigned b)
+{
+	while (b != 0)
+	{
+		std::swap(a, b);
+		b = b % a;
+	}
+	return (a != 0) ? a : 1;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 1. Реализовать метод ToDouble() согласно заданию
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 2. Реализовать унарный + и унарный -
+//////////////////////////////////////////////////////////////////////////
+
+CRational const CRational::operator-() const
+{
+	return CRational(-m_numerator, m_denominator);
+}
+
+CRational const CRational::operator+() const
+{
+	return *this;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 3. Реализовать бинарный +
+//////////////////////////////////////////////////////////////////////////
+
+CRational const CRational::operator +(CRational const &rat) const
+{
+	auto copyRightVal = rat;
+	auto copyLeftVal = std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= m_denominator;
+	copyRightVal.m_denominator *= m_denominator;
+	copyLeftVal.first *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.second *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.first += copyRightVal.m_numerator;
+	auto mostLarDiv = GCD(copyLeftVal.first, copyLeftVal.second);
+	return{ copyLeftVal.first / int(mostLarDiv), copyLeftVal.second / int(mostLarDiv) };
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 4. Реализовать бинарный -
+//////////////////////////////////////////////////////////////////////////
+CRational const CRational::operator -(CRational const &rat) const
+{
+	auto copyRightVal = rat;
+	auto copyLeftVal = std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= m_denominator;
+	copyRightVal.m_denominator *= m_denominator;
+	copyLeftVal.first *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.second *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.first -= copyRightVal.m_numerator;
+	auto mostLarDiv = GCD(copyLeftVal.first, copyLeftVal.second);
+	return { copyLeftVal.first / int(mostLarDiv), copyLeftVal.second / int(mostLarDiv) };
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 5. Реализовать оператор +=
+//////////////////////////////////////////////////////////////////////////
+
+void const CRational::operator +=(CRational const &rat) 
+{
+	auto res = CRational(m_numerator, m_denominator) + rat;
+	m_numerator = res.m_numerator;
+	m_denominator = res.m_denominator;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 6. Реализовать оператор -=
+//////////////////////////////////////////////////////////////////////////
+
+void const CRational::operator -=(CRational const &rat)
+{
+	auto res = CRational(m_numerator, m_denominator) - rat;
+	m_numerator = res.m_numerator;
+	m_denominator = res.m_denominator;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 7. Реализовать оператор *
+//////////////////////////////////////////////////////////////////////////
+
+CRational const operator *(CRational const &rat1, CRational const &rat2)
+{
+	auto copyRightVal = rat1;
+	//auto copyLeftVal = std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= rat2.m_numerator;
+	copyRightVal.m_denominator *= rat2.m_denominator;
+	auto mostLarDiv = GCD(copyRightVal.m_numerator, copyRightVal.m_denominator);
+	return CRational(copyRightVal.m_numerator / int(mostLarDiv), copyRightVal.m_denominator / int(mostLarDiv));
+}
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 8. Реализовать оператор /
+//////////////////////////////////////////////////////////////////////////
+
+
+CRational const CRational::operator /(CRational const &rat) const
+{
+	auto copy = std::pair<int, int>(m_numerator, m_denominator);
+	copy.first *= rat.m_denominator;
+	copy.second *= rat.m_numerator;
+	auto mostLarDiv = GCD(copy.first, copy.second);
+	return CRational(copy.first / int(mostLarDiv), copy.second / int(mostLarDiv));
+}
+
+CRational const operator /(int t, CRational const &rat)
+{
+	return CRational(rat.m_denominator * t, rat.m_numerator);
+}
+
+CRational const operator /(CRational const &rat, int t)
+{
+	return CRational(rat.m_numerator, rat.m_denominator * t);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 9. Реализовать оператор *=
+//////////////////////////////////////////////////////////////////////////
+
+void const CRational::operator *=(CRational const &rat)
+{
+	auto res = CRational(m_numerator, m_denominator) * rat;
+	m_numerator = res.m_numerator;
+	m_denominator = res.m_denominator;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 10. Реализовать оператор /=
+//////////////////////////////////////////////////////////////////////////
+
+void const CRational::operator /=(CRational const &rat)
+{
+	auto res = CRational(m_numerator, m_denominator) / rat;
+	m_numerator = res.m_numerator;
+	m_denominator = res.m_denominator;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 11. Реализовать операторы == и !=
+//////////////////////////////////////////////////////////////////////////
+bool const CRational::operator ==(CRational const &rat) const
+{
+	return (m_numerator == rat.m_numerator && m_denominator == rat.m_denominator);
+}
+
+bool const CRational::operator !=(CRational const &rat) const
+{
+	return (m_numerator != rat.m_numerator || m_denominator != rat.m_denominator);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 12. Реализовать операторы <, >, <=, >=
+//////////////////////////////////////////////////////////////////////////
+bool const CRational::operator <(CRational const &rat) const
+{
+	auto copyRightVal = rat;
+	auto copyLeftVal = *this;//std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= m_denominator;
+	copyRightVal.m_denominator *= m_denominator;
+	copyLeftVal.m_numerator *= copyRightVal.m_denominator / copyLeftVal.m_denominator;
+	copyLeftVal.m_denominator *= copyRightVal.m_denominator / copyLeftVal.m_denominator;
+	return copyLeftVal.m_numerator < copyRightVal.m_numerator;
+}
+
+bool const CRational::operator >(CRational const &rat) const
+{
+	auto copyRightVal = rat;
+	auto copyLeftVal = std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= m_denominator;
+	copyRightVal.m_denominator *= m_denominator;
+	copyLeftVal.first *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.second *= copyRightVal.m_denominator / copyLeftVal.second;
+	return copyLeftVal.first > copyRightVal.m_numerator;
+}
+
+bool const CRational::operator <=(CRational const &rat) const
+{
+	auto copyRightVal = rat;
+	auto copyLeftVal = std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= m_denominator;
+	copyRightVal.m_denominator *= m_denominator;
+	copyLeftVal.first *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.second *= copyRightVal.m_denominator / copyLeftVal.second;
+	return copyLeftVal.first <= copyRightVal.m_numerator;
+}
+
+bool const CRational::operator >=(CRational const &rat) const
+{
+	auto copyRightVal = rat;
+	auto copyLeftVal = std::pair<int, int>(m_numerator, m_denominator);
+	copyRightVal.m_numerator *= m_denominator;
+	copyRightVal.m_denominator *= m_denominator;
+	copyLeftVal.first *= copyRightVal.m_denominator / copyLeftVal.second;
+	copyLeftVal.second *= copyRightVal.m_denominator / copyLeftVal.second;
+	return copyLeftVal.first >= copyRightVal.m_numerator;
+}
+//////////////////////////////////////////////////////////////////////////
+// TODO: 13. Реализовать оператор вывода рационального числа в выходной поток 
+//////////////////////////////////////////////////////////////////////////
+
+
+std::ostream& operator << (std::ostream &os, CRational const &rat)
+{
+	return os << rat.m_numerator << "/" << rat.m_denominator;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 14. Реализовать оператор ввода рационального числа из входного потока 
+//////////////////////////////////////////////////////////////////////////
+
+
+/*std::istream& operator >> (std::istream &is, CRational &rat)
+{
+	std::string str;
+	is >> str;
+	std::vector<std::string> vec;
+	boost::split(vec, str, boost::is_any_of("/"));
+	rat.m_numerator = atoi(vec[0].c_str());
+	rat.m_denominator = atoi(vec[1].c_str());
+	return is;
+}*/
---
+++ Lab5/lab5/task1tests/RationalTests.cpp
@@ -0,0 +1,336 @@
+﻿// task1tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../task1/Rational.h"
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(Test_Greates_Common_Denominator)
+{
+	BOOST_CHECK_EQUAL(GCD(2, 6), 2u);
+	BOOST_CHECK_EQUAL(GCD(3, 2), 1u);
+	BOOST_CHECK_EQUAL(GCD(12, 8), 4u);
+	BOOST_CHECK_EQUAL(GCD(8, 12), 4u);
+	BOOST_CHECK_EQUAL(GCD(0, 2), 2u);
+	BOOST_CHECK_EQUAL(GCD(2, 0), 2u);
+	BOOST_CHECK_EQUAL(GCD(0, 0), 1u);
+}
+
+
+
+/*
+  Рациональное число:
+	равно нулю по умолчанию (0/1)
+	может быть созданно из целого в формате (n / 1)
+	может быть задано с указанием числителя и знаменателя
+	хранится в нормализованном виде
+	*/
+
+void VerifyRational(const CRational & r, int expectedNumerator, int expectedDenominator)
+{
+	std::cout << "expectedNumerator = " << expectedNumerator << std::endl;
+	std::cout << "expectedDenominator = " << expectedDenominator << std::endl;
+	std::cout << "------------------------------------------ = " << std::endl;
+	BOOST_CHECK_EQUAL(r.GetNumerator(), expectedNumerator);
+	BOOST_CHECK_EQUAL(r.GetDenominator(), expectedDenominator);
+}
+
+BOOST_AUTO_TEST_SUITE(Rational_number)
+	BOOST_AUTO_TEST_CASE(is_0_by_default)
+	{
+		VerifyRational(CRational(), 0, 1);
+	}
+	BOOST_AUTO_TEST_CASE(can_be_constructed_from_integer)
+	{
+		VerifyRational(CRational(10), 10, 1);
+		VerifyRational(CRational(-10), -10, 1);
+		VerifyRational(CRational(0), 0, 1);
+	}
+	BOOST_AUTO_TEST_CASE(can_be_constructed_with_numerator_and_denominator)
+	{
+		VerifyRational(CRational(5, 2), 5, 2);
+		VerifyRational(CRational(-5, 2), -5, 2);
+		VerifyRational(CRational(5, -2), -5, 2);
+		VerifyRational(CRational(-5, -2), 5, 2);
+	}
+	BOOST_AUTO_TEST_CASE(is_normalized_after_construction)
+	{
+		VerifyRational(CRational(6, 8), 3, 4);
+		VerifyRational(CRational(6, -8), -3, 4);
+		VerifyRational(CRational(-6, 8), -3, 4);
+		VerifyRational(CRational(-6, -8), 3, 4);
+		VerifyRational(CRational(-10, 20), -1, 2);
+	}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 1. Реализовать метод ToDouble() согласно заданию
+// Возвращает отношение числителя и знаменателя в виде числа double
+// Пример использования:
+//	CRational r(3, 5)
+//	cout << r.ToDouble(); // Должно вывести 0.6
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 2. Реализовать унарный + и унарный -
+// Указание: см. материалы к лекции
+// Пример использования:
+//	const CRational r1(3, 5);
+//	CRational r2 = -r1; // r2 должно стать -3/5
+//	assert(r2.GetNumerator(), -3);
+//	assert(r2.GetDenominator(), 5);
+//	CRational r3 = +r2; // r3 также равно -3/5
+//	assert(r3.GetNumerator(), -3);
+//	assert(r3.GetDenominator(), 5);
+// Унарный минус возвращает раицональное число без знака
+// Унарный плюс возвращает рациональное число, равное текущему
+// Реализация не должна допускать операции вроде:
+//  -someRational = someOtherRational;
+//	+someRational = someOtherRational;
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(unary_minus)
+	{
+		CRational r1(3, 5);
+		CRational r2 = -r1;
+		BOOST_CHECK_EQUAL(r2.GetNumerator(), -3);
+		BOOST_CHECK_EQUAL(r2.GetDenominator(), 5);
+	}
+
+	BOOST_AUTO_TEST_CASE(unary_plus)
+	{
+		CRational r1(3, 5);
+		CRational r2 = -r1;
+		CRational r3 = +r2;
+		BOOST_CHECK_EQUAL(r3.GetNumerator(), -3);
+		BOOST_CHECK_EQUAL(r3.GetDenominator(), 5);
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 3. Реализовать бинарный +
+// Возвращает результат сложения двух рациональных чисел, 
+//	рационального числа с целым, целого числа с рациональным.
+//	(1/2) + (1/6) = (2/3)
+//	(1/2) + 1     = (3/2)
+//	1 + (1/2)     = (3/2)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_binary_plus)
+	{
+		VerifyRational(CRational(1, 2) + CRational(1, 6), 2, 3);
+		VerifyRational(CRational(1, 2) + CRational(1), 3, 2);
+		VerifyRational(CRational(1) + CRational(1, 2), 3, 2);
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 4. Реализовать бинарный -
+// Возвращает разность двух рациональных чисел, 
+//	рационального числа и целого, либо целого числа и рационального:
+//	(1/2) - (1/6) = (1/3)
+//	(1/2) - 1     = (-1/2)
+//	1 - (1/2)     = (1/2)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_binary_minus)
+	{
+		VerifyRational(CRational(1, 2) - CRational(1, 6), 1, 3);
+		VerifyRational(CRational(1, 2) - CRational(1), -1, 2);
+		VerifyRational(CRational(1) - CRational(1, 2), 1, 2);
+	}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 5. Реализовать оператор +=
+// Выполняет увеличение рационального числа на величину второго рационального,
+//	либо целого числа:
+//	(1/2) += (1/6)  → (2/3)
+//	(1/2) += 1      → (3/2)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_plus_the_same)
+	{
+		CRational rat(1, 2);
+		rat += CRational(1, 6);
+		VerifyRational(rat, 2, 3);
+		CRational rat2(1, 2);
+		rat2 += 1;
+		VerifyRational(rat2, 3, 2);
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 6. Реализовать оператор -=
+// Выполняет уменьшение рационального числа на величину второго рационального либо целого числа :
+// (1/2) -= (1/6)  → (1/3)
+// (1/2) -= 1      → (-1/2)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_minus_the_same)
+	{
+		CRational rat(1, 2);
+		rat -= CRational(1, 6);
+		VerifyRational(rat, 1, 3);
+		CRational rat2(1, 2);
+		rat2 -= 1;
+		VerifyRational(rat2, -1, 2);
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 7. Реализовать оператор *
+// Возвращает результат произведения рациональных чисел, 
+//	рационального и целого, либо целого и рационального :
+//	(1/2) * (2/3) = (1/3)
+//	(1/2) * (-3)  = (-3/2)
+//	(7*2) / 3     = (14/3)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_multiplication_sign)
+	{
+		VerifyRational(CRational(1, 2) * CRational(2, 3), 1, 3);
+		VerifyRational(CRational(1, 2) * CRational(-3), -3, 2);
+		VerifyRational(CRational(2, 3) * 7, 14, 3);
+		VerifyRational(7 * CRational(2, 3), 14, 3);
+	}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 8. Реализовать оператор /
+// Возвращает частное двух рациональных чисел, 
+//	рационального и целого, целого и рационального :
+//	(1/2) ⁄ (2/3) = (3/4)
+//	(1/2) ⁄ 5     = (1/10)
+//	7 ⁄ (2/3)     = (21/2)
+//////////////////////////////////////////////////////////////////////////
+
+
+	BOOST_AUTO_TEST_CASE(work_with_the_division_sign)
+	{
+		VerifyRational(CRational(1, 2) / CRational(2, 3), 3, 4);
+		VerifyRational(CRational(1, 2) / CRational(5), 1, 10);
+		VerifyRational(CRational(2, 3) / 7, 2, 21);
+		VerifyRational(7 / CRational(2, 3), 21, 2);
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 9. Реализовать оператор *=
+// Умножает значение первого рационального числа на другое рациональное, 
+//	либо целое:
+//	(1/2) *= (2/3) → (1/3)
+//	(1/2) *= 3     → (3/2)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_multiplication_the_same)
+	{
+		CRational rat(1, 2);
+		rat *= CRational(2, 3);
+		VerifyRational(rat, 1, 3);
+		CRational rat2(1, 2);
+		rat2 *= 3;
+		VerifyRational(rat2, 3, 2);
+	}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 10. Реализовать оператор /=
+// Делит первое рациональное число на другое рациональное, 
+//	либо целое:
+//	(1/2) /= (2/3) → (3/4)
+//	(1/2) /= 3     → (1/6)
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(work_with_the_division_the_same)
+	{
+		CRational rat(1, 2);
+		rat /= CRational(2, 3);
+		VerifyRational(rat, 3, 4);
+		CRational rat2(1, 2);
+		rat2 /= 3;
+		VerifyRational(rat2, 1, 6);
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 11. Реализовать операторы == и !=
+// Проверяют равенство (и неравенство) двух рациональных чисел, 
+//	целого и рационального, рационального и целого:
+//	(1/2) == (1/2) → true
+//	(4/1) == 4     → true
+//	3 == (3/1)     → true
+//	(1/2) != (2/3) → true
+//	(1/2) != 7     → true
+//	3 != (2/3)     → true
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_CASE(equality_and_inequality)
+	{
+		BOOST_CHECK(CRational(1, 2) == CRational(1, 2));
+		BOOST_CHECK(CRational(4, 1) == CRational(4));
+		BOOST_CHECK(CRational(3) == CRational(3, 1));
+		BOOST_CHECK(CRational(1, 2) != CRational(2, 3));
+		BOOST_CHECK(CRational(1, 2) != CRational(7));
+		BOOST_CHECK(CRational(3) != CRational(2, 3));
+	}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 12. Реализовать операторы <, >, <=, >=
+// Сравнивают два рациональных числа, рациональное с целым, 
+//	целое с рациональным:
+//	(1/2) >= (1/3) → true
+//	(1/2) <= (1/3) → false
+//	(3/1) > 2      → true
+//	(1/2) < 7      → true
+//	3 <= (7/2)     → true
+//	3 >= (8/2)     → false
+//////////////////////////////////////////////////////////////////////////
+	BOOST_AUTO_TEST_CASE(compare)
+	{
+		BOOST_CHECK(CRational(1, 2) >= CRational(1, 3));
+		BOOST_CHECK(!(CRational(1, 2) <= CRational(1, 3)));
+		BOOST_CHECK(CRational(3, 1) > CRational(2));
+		BOOST_CHECK(CRational(1, 2) < CRational(7));
+		BOOST_CHECK(CRational(3) <= CRational(7, 2));
+		BOOST_CHECK(!(CRational(1, 2) >= CRational(8, 2)));
+	}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 13. Реализовать оператор вывода рационального числа в выходной поток 
+//	std::ostream в формате <числитель>/<знаменатель>, 
+//	например: 7/15
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 14. Реализовать оператор ввода рационального числа из входного потока 
+//	std::istream в формате <числитель>/<знаменатель>, 
+//	например: 7/15
+//////////////////////////////////////////////////////////////////////////
+	BOOST_AUTO_TEST_CASE(entry_operator)
+	{
+		CRational rat3;
+		CRational rat4;
+		std::cin >> rat3;
+		std::cin >> rat4;
+		std::cout << rat3;
+		std::cout << rat4;
+	}
+
+
+BOOST_AUTO_TEST_SUITE_END()
